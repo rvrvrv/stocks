3f551da4520e7d2abf1535c8d6f2f19eaa157133
Rich <ibanez19@sbcglobal.net>
2017-07-07 20:52:57 +0000

    Initialize tooltips

--- public/js/main.js
+++ public/js/main.js
@@ -1,108 +1,12 @@
 /*jshint browser: true, esversion: 6*/
-/* global $, Highcharts, io */
+/* global $, Highcharts, io, Materialize */
+
+//Global vars
+const socket = io();
+var stockData = {};
+var chart;
 
 $(document).ready(() => {
-	
-	//Global vars
-	var socket = io();
-	var stockData = {};
-	var chart;
-	
-	//Generate materialize card and modal for stocks
-	function generateHTML(stock) {
-		//HTML for modal
-		$('.modals').append(`
-			<div id="del${stock}" class="modal">
-				<div class="modal-content">
-					<h4>Delete ${stock}</h4>
-					<p>Are you sure you want to remove ${stock}?</p>
-				</div>
-				<div class="modal-footer">
-					<a class="modal-action modal-close waves-effect waves-green btn-flat">No</a>
-					<a class="modal-action modal-close waves-effect waves-red btn-flat del-btn" data-stock="${stock}">Yes</a>
-				</div>
-			</div>`);
-		$('.modal').modal();
-
-		//HTML for card
-		$('.stock-cards').append(`
-			<div class="col s6 m4" id="${stock}">
-				<div class="card grey darken-4 z-depth-3">
-					<div class="card-content white-text">
-						<span class="card-title b">${stock}</span>
-						<a class="btn-floating waves-effect tooltipped indigo darken-3" data-position="bottom" data-delay="100" data-tooltip="Learn about ${stock}" href="https://www.google.com/finance?q=${stock}" target="_blank"><i class="large material-icons">info</i></a>
-						<a class="btn-floating modal-trigger waves-effect tooltipped red darken-4" data-position="bottom" data-delay="100" data-tooltip="Remove ${stock}" href="#del${stock}"><i class="large material-icons">delete</i></a>
-					</div>
-				</div>
-			</div>`);
-	}
-
-	//Remove stock from the list and chart
-	function removeStock(stock) {
-		//First, ensure stock exists on page
-		if ($(`#${stock}`).length < 1) return;
-		//Remove stock card, its modal, and chart data
-		$(`#${stock}`).remove();
-		$(`#del${stock}`).remove();
-		chart.get(stock).remove();
-	}
-
-	//Add stock to the list and chart
-	function addStock(stockObj) {
-		//First, ensure stock data is valid
-		if (!stockObj) return enableInput('No stock data found.');
-
-		//Then, ensure stock isn't already on page
-		let stock = Object.keys(stockObj)[0];
-		if ($(`#${stock}`).length) return;
-
-		//Add stock card and modal
-		generateHTML(stock);
-
-		//Add stock data to chart
-		chart.addSeries({
-			name: stock,
-			id: stock,
-			data: stockObj[stock]
-		});
-		enableInput();
-	}
-
-	//Enable the input field and optionally, display an error message
-	function enableInput(errorMsg) {
-		if (errorMsg) Materialize.toast(errorMsg, 3500, 'red darken-2');
-		$('#addStock :input').prop('disabled', false);
-		$('.progress').addClass('hidden');
-		chart.hideLoading();
-	}
-
-	//Handle 'delete stock' confirmation
-	$('.modals').on('click', '.del-btn', function () {
-		let stock = $(this).attr('data-stock');
-		//Remove stock from client's page
-		removeStock(stock);
-		/* Remove stock from list on server,
-		which will also update all other clients */
-		socket.emit('deleteStock', stock);
-	});
-
-	//Handle submission from 'add a stock' field
-	$('#addStock').on('submit', e => {
-		e.preventDefault();
-		chart.showLoading();
-		$('.progress').removeClass('hidden');
-		$('#addStock :input').prop('disabled', true);
-		let stock = $('#stockInput').val().trim().toUpperCase();
-		//Ensure input contains letters only
-		if (/[^A-Z]/.test(stock) || !stock)
-			return enableInput('Please enter a valid ticker symbol');
-		//Ensure stock isn't already on page
-		if ($(`#${stock}`).length > 0)
-			return enableInput(`${stock} is already on the chart.<br> Please enter a new ticker symbol.`);
-		//If the ticker symbol is valid, try to add it on the server
-		socket.emit('addStock', stock);
-	});
-
 	/*******************************
 	BEGIN socket.io operations
 	*******************************/
@@ -139,15 +43,112 @@ $(document).ready(() => {
 	/*******************************
 	END socket.io operations
 	*******************************/
+});
 
+
+//Generate materialize card and modal for stocks
+function generateHTML(stock) {
+	//HTML for modal
+	$('.modals').append(`
+			<div id="del${stock}" class="modal">
+				<div class="modal-content">
+					<h4>Delete ${stock}</h4>
+					<p>Are you sure you want to remove ${stock}?</p>
+				</div>
+				<div class="modal-footer">
+					<a class="modal-action modal-close waves-effect waves-green btn-flat">No</a>
+					<a class="modal-action modal-close waves-effect waves-red btn-flat del-btn" data-stock="${stock}">Yes</a>
+				</div>
+			</div>`);
+	$('.modal').modal();
+
+	//HTML for card
+	$('.stock-cards').append(`
+			<div class="col s6 m4" id="${stock}">
+				<div class="card grey darken-4 z-depth-3">
+					<div class="card-content white-text">
+						<span class="card-title b">${stock}</span>
+						<a class="btn-floating waves-effect tooltipped indigo darken-3" data-position="bottom" data-tooltip="Learn about ${stock}" href="https://www.google.com/finance?q=${stock}" target="_blank"><i class="material-icons">info</i></a>
+						<a class="btn-floating modal-trigger waves-effect tooltipped red darken-4" data-position="bottom" data-tooltip="Remove ${stock}" href="#del${stock}"><i class="material-icons">delete</i></a>
+					</div>
+				</div>
+			</div>`);
+	$('.tooltipped').tooltip({delay: 100});
+}
+
+//Remove stock from the list and chart
+function removeStock(stock) {
+	//First, ensure stock exists on page
+	if ($(`#${stock}`).length < 1) return;
+	//Remove stock card, its modal, and chart data
+	$(`#${stock}`).remove();
+	$(`#del${stock}`).remove();
+	chart.get(stock).remove();
+}
+
+//Add stock to the list and chart
+function addStock(stockObj) {
+	//First, ensure stock data is valid
+	if (!stockObj) return enableInput('No stock data found.');
+
+	//Then, ensure stock isn't already on page
+	let stock = Object.keys(stockObj)[0];
+	if ($(`#${stock}`).length) return;
+
+	//Add stock card and modal
+	generateHTML(stock);
+
+	//Add stock data to chart
+	chart.addSeries({
+		name: stock,
+		id: stock,
+		data: stockObj[stock]
+	});
+	enableInput();
+}
+
+//Enable the input field and optionally, display an error message
+function enableInput(errorMsg) {
+	if (errorMsg) Materialize.toast(errorMsg, 3500, 'red darken-2');
+	$('#addStock :input').prop('disabled', false);
+	$('.progress').addClass('hidden');
+	chart.hideLoading();
+}
+
+//Handle 'delete stock' confirmation
+$('.modals').on('click', '.del-btn', function() {
+	let stock = $(this).attr('data-stock');
+	//Remove stock from client's page
+	removeStock(stock);
+	/* Remove stock from list on server,
+	which will also update all other clients */
+	socket.emit('deleteStock', stock);
+});
+
+//Handle submission from 'add a stock' field
+$('#addStock').on('submit', e => {
+	e.preventDefault();
+	chart.showLoading();
+	$('.progress').removeClass('hidden');
+	$('#addStock :input').prop('disabled', true);
+	let stock = $('#stockInput').val().trim().toUpperCase();
+	//Ensure input contains letters only
+	if (/[^A-Z]/.test(stock) || !stock)
+		return enableInput('Please enter a valid ticker symbol');
+	//Ensure stock isn't already on page
+	if ($(`#${stock}`).length > 0)
+		return enableInput(`${stock} is already on the chart.<br> Please enter a new ticker symbol.`);
+	//If the ticker symbol is valid, try to add it on the server
+	socket.emit('addStock', stock);
 });
 
 //Chart config
 var seriesOptions = [],
 	chartConfig = {
 		colors: ['#55b209', '#003aff', '#ff5319', '#ffc200',
-					'#b21409', '#a712b2', '#09b28d', '#6818cc',
-					'#b29900', '#49f6ff', '#009cff', '#cc2323'],
+			'#b21409', '#a712b2', '#09b28d', '#6818cc',
+			'#b29900', '#49f6ff', '#009cff', '#cc2323'
+		],
 		chart: {
 			backgroundColor: '#212121',
 			style: {
@@ -166,7 +167,7 @@ var seriesOptions = [],
 			value: 0,
 			width: 2,
 			color: 'silver'
-            }],
+		}],
 		plotOptions: {
 			series: {
 				compare: 'percent',
@@ -200,7 +201,7 @@ var seriesOptions = [],
 			tickColor: '#A2A39C',
 			tickWidth: 1,
 			labels: {
-				formatter: function () {
+				formatter: function() {
 					return (this.value > 0 ? ' + ' : '') + this.value + '%';
 				}
 			},
